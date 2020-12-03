3dbd88b907ef2ea8fe90e7f19123c642622ba90f
Nicolas Ganz <gann@zhaw.ch>
2020-12-03 17:34:30 +0100

    streamline the printer and removed static variables

--- Book_Invoicings_Example/SOLID/src/srp/good/InvoicePersistence.java
+++ Book_Invoicings_Example/SOLID/src/srp/good/InvoicePersistence.java
@@ -3,7 +3,7 @@ package srp.good;
 class InvoicePersistence {
   public void save(Invoice invoice) {
     // Implement File save logic
-    System.out.print(invoice.book.name + " saved in File.");
+    System.out.print(invoice.book.name + " saved into File.");
     System.out.println("\n");
   }
 }
--- Book_Invoicings_Example/SOLID/src/srp/good/InvoicePrinter.java
+++ Book_Invoicings_Example/SOLID/src/srp/good/InvoicePrinter.java
@@ -2,14 +2,11 @@ package srp.good;
 
 // Handles printing logic
 class InvoicePrinter {
-  private Invoice invoice;
-
-  public InvoicePrinter(Invoice invoice){
-    this.invoice = invoice;
+  public InvoicePrinter(){
   }
 
   // method that should print the invoicing
-  public void printInvoice() {
+  public void printInvoice(Invoice invoice) {
     System.out.println("Invoicing: " + invoice.quantity + " x " + invoice.book.name + " for " + invoice.book.price + "$");
     System.out.println("Discount Rate: " + invoice.discountRate);
     System.out.println("Tax Rate: " + invoice.taxRate);
